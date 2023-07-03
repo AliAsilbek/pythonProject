75f7ad2597374a8c4f702de66bee0c2c7e525ccc
Али <duper0911@gmail.com>
2023-07-03 14:50:30 +0600

    Initial commit

---
+++ bornday.py
@@ -0,0 +1,11 @@
+
+age = input('what year was A.S. Pushkin born? ')
+while not age == '1799':
+    print('incorrect')
+    age = input('what year was A.S. Pushkin born? ')
+
+day = input('what month and day was A.S. Pushkin born? ')
+while not day == "6 june":
+    print('incorrect')
+    day = input('what month and day was A.S. Pushkin born? (day month) ')
+print('correct')
---
+++ bornyear.py
@@ -0,0 +1,4 @@
+age = ''
+while not age == "1799":
+    age = input('what year was A.S. Pushkin born? ')
+print('correct')
